b7f3af1422576401a2ae3d327fdb169df6ab6bac
jmogo <jmogovan2667@gmail.com>
2025-05-07 20:11:27 +0300

    fixed migrations

---
+++ NeoIsisJob/Workout.Core/Migrations/20250507170554_ChangedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Workout.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PersonalTrainers",
+                keyColumn: "PTID",
+                keyValue: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PersonalTrainers",
+                columns: new[] { "PTID", "FirstName", "LastName", "WorksSince" },
+                values: new object[] { 3, "Mihai", "Predescu", new DateTime(2022, 5, 11, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
