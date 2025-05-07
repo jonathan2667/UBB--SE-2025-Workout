861d59b5c319c4a40b4d0076b5944ad954c0ef66
PanCat26 <catalin.pangaleanu@gmail.com>
2025-05-07 17:26:37 +0300

    Added repository interface for shop repositories.

---
+++ NeoIsisJob/Workout.Core/Utils/Filters/CartItemFilter.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="CartItemFilter.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WorkoutApp.Utils.Filters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a filter interface that can be implemented to define filtering logic.
+    /// </summary>
+    public class CartItemFilter(int? productID, int? customerID): IFilter
+    {
+        /// <summary>
+        /// Gets or sets product id to filter by.
+        /// </summary>
+        public int? ProductID { get; set; } = productID;
+
+        /// <summary>
+        /// Gets or sets customer id to filter by.
+        /// </summary>
+        public int? CustomerID { get; set; } = customerID;
+    }
+}
---
+++ NeoIsisJob/Workout.Core/Utils/Filters/IFilter.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IFilter.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WorkoutApp.Utils.Filters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a filter interface that can be implemented to define filtering logic.
+    /// </summary>
+    public interface IFilter
+    {
+    }
+}
