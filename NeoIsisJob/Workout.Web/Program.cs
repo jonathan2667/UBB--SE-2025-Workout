f7c7a3757d7e49a3bffcbea14833986eaa462800
Nicola Razvan <146840683+915-Nicola-Razvan-Cristian@users.noreply.github.com>
2025-05-12 22:31:51 +0300

    Added example code for controller and view (#39)
    * Created web server app
    
    * Model for controller

--- NeoIsisJob/Workout.Web/Program.cs
+++ NeoIsisJob/Workout.Web/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Workout.Web.Data;
+using Workout.Core.Data;
+using Workout.Core.IRepositories;
+using Workout.Core.Repositories;
+using Workout.Core.IServices;
+using Workout.Core.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +14,20 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add WorkoutDbContext
+builder.Services.AddDbContext<WorkoutDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add repositories
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<IClassRepository, ClassRepository>();
+builder.Services.AddScoped<IUserClassRepo, UserClassRepo>();
+
+// Add services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IClassService, ClassService>();
+builder.Services.AddScoped<IUserClassService, UserClassService>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
