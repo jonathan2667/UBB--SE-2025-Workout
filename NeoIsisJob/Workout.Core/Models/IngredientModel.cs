0d07183f1a5e9e5567ef1aaacbf025f4df091d5d
Mol-Anita <molanita1121@gmail.com>
2025-05-28 14:47:35 +0300

    feat: added Meal and Ingredient models and relations into DbContext

---
+++ NeoIsisJob/Workout.Core/Models/IngredientModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Workout.Core.Models
+{
+    public class IngredientModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<MealModel> Meals { get; set; }
+    }
+}
---
+++ NeoIsisJob/Workout.Core/Models/MealModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workout.Core.Models
+{
+    public class MealModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string ImageUrl { get; set; }
+        public string CookingLevel { get; set; }
+        public int CookingTimeMins { get; set; }
+        public string Directions { get; set; }
+        public List<IngredientModel> Ingredients { get; set; }
+    }
+}
